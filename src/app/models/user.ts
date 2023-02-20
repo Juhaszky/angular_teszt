593473234dce8e43afc792fdf024e5d6a4d9850b
juhasz.krisztian <juhasz.krisztian@fornax.hu>
2023-02-20 12:49:50 +0100

    User model modified

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -2,12 +2,10 @@ export class User {
     email: string;
     password: string;
     cryptos: string[];
-    isLogged: boolean;
 
     constructor(email: string, password: string, cryptos: string[]) {
         this.email = email;
         this.password = password;
         this.cryptos = cryptos;
-        this.isLogged = false;
     }
 }
