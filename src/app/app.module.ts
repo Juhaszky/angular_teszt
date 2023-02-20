eedaac637ebf387d5390fbffee7163186c8426d9
juhasz.krisztian <juhasz.krisztian@fornax.hu>
2023-02-20 12:52:59 +0100

    dashboard component created, basic design added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,14 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
