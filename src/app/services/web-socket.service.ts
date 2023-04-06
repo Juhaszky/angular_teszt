8de80986df73330ed89460bf70dc9dcc054d885a
juhasz.krisztian <juhasz.krisztian@fornax.hu>
2023-04-06 15:48:43 +0200

    WebSocket service implement

---
+++ src/app/services/web-socket.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
+
+interface MessageData {
+  message: string;
+  time?: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebSocketService {
+
+  constructor() { }
+
+  private socket$!: WebSocketSubject<any>;
+  public receivedData: MessageData[] = [];
+
+  public connect(): void {
+    if (!this.socket$ || this.socket$.closed) {
+      this.socket$ = webSocket('wss://ws-sandbox.coinapi.io/v1/');
+      console.log("done");
+      this.socket$.subscribe((data: MessageData) => {
+        this.receivedData.push(data);
+      });
+    }
+  }
+
+  sendMessage(message: string) {
+    this.socket$.next(message);
+  }
+
+  close() {
+    this.socket$.complete();
+  }
+}
